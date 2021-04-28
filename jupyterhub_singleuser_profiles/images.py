911782ccdfdad5fa977b135540f3b2b94489f4bf
Václav Pavlín <vaclav.pavlin@gmail.com>
2021-04-28 08:59:31 +0200

    Refactor Images

--- jupyterhub_singleuser_profiles/images.py
+++ jupyterhub_singleuser_profiles/images.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import logging
 import json
+from distutils.util import strtobool
 from pydantic import BaseModel
 from typing import List, Optional
 
@@ -13,7 +14,6 @@ DISPLAY_NAME_ANNOTATION = 'opendatahub.io/notebook-image-name'
 URL_ANNOTATION = 'opendatahub.io/notebook-image-url'
 SOFTWARE_ANNOTATION = 'opendatahub.io/notebook-software'
 DEPENDENCIES_ANNOTATION = 'opendatahub.io/notebook-python-dependencies'
-IMAGE_ORDER_ANNOTATION = 'opendatahub.io/image-order'
 
 
 class NameVersionPair(BaseModel):
@@ -31,66 +31,48 @@ class ImageInfo(BaseModel):
     name: str
     content: ImageTagInfo
     default: bool = False
-    order: int = 100
 
 class Images(object):
     def __init__(self, openshift, namespace):
         self.openshift = openshift
         self.namespace = namespace
 
-
-    def get_images_legacy(self, result):
-        """Kept for backwards compatibility"""
-
-        for i in self.openshift.get_imagestreams().items:
-            if '-notebook' in i.metadata.name:
-                self.append_option(i, result)
-
     def get_default(self):
-        image_list, code = self.get(detailed=False)
+        image_list = self.load()
 
-        if len(image_list) > 0:
-            return image_list[0]
+        for image in image_list:
+            if image.default:
+                return image.name
 
-        return ''
+        return image_list[0].name if len(image_list) else None
 
     def tag_exists(self, tag_name, imagestream):
-        for tag in imagestream.spec.tags:
-            if tag_name == tag.name:
+        for tag in imagestream.status.tags:
+            if tag_name == tag.tag:
                 return True
 
         return False
-
-    def check_place(self, imagestream):
-        return imagestream.order
 
     def load(self):
         result = []
         imagestream_list = self.openshift.get_imagestreams(IMAGE_LABEL+'=true')
 
         for i in imagestream_list.items:
-            if i.metadata.name == name:
-                annotations = i.metadata.annotations
-                for tag in i.spec.tags:
-                    if tag.name == tag_name:
-                        tag_annotations = tag.annotations
-                if not tag_annotations:
-                    _LOGGER.error("Image tag not found!")
-                    return "Image tag not found", 404
-
-                return ImageInfo(description=annotations.get(desc),
-                                    url=annotations.get(url),
-                                    display_name=annotations.get(display_name),
-                                    name=image_name,
+            tag_annotations = {}
+            annotations = i.metadata.annotations
+            for tag in i.spec.tags:
+                if not self.tag_exists(tag.name, i):
+                    continue
+                result.append(ImageInfo(description=annotations.get(DESCRIPTION_ANNOTATION),
+                                    url=annotations.get(URL_ANNOTATION),
+                                    display_name=annotations.get(DISPLAY_NAME_ANNOTATION),
+                                    name="%s:%s" % (i.metadata.name, tag.name),
                                     content=ImageTagInfo(
                                         software=json.loads(tag.annotations.get(SOFTWARE_ANNOTATION, "[]")),\
                                         dependencies=json.loads(tag.annotations.get(DEPENDENCIES_ANNOTATION, "[]"))
                                     ),
-                                    default=bool(strtobool(annotations.get(DEFAULT_IMAGE_ANNOTATION, "False"))),
-                                    order=int(annotations.get(IMAGE_ORDER_ANNOTATION, 100))
+                                    default=bool(strtobool(annotations.get(DEFAULT_IMAGE_ANNOTATION, "False")))
                                     ))
-
-        result.sort(key=self.check_place)
 
         return result
 
