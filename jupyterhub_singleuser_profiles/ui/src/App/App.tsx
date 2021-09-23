4be4e37a1d2fb93bed4a5a1e6ea7990610e5e771
Jeff Phillips <jephilli@redhat.com>
2021-09-23 11:23:35 -0400

    Watch available images, don't enable Start until one is selected (#197)

--- jupyterhub_singleuser_profiles/ui/src/App/App.tsx
+++ jupyterhub_singleuser_profiles/ui/src/App/App.tsx
@@ -22,8 +22,17 @@ import './App.scss';
 const App: React.FC = () => {
   const [uiConfig, setUiConfig] = React.useState<UiConfigType>();
   const [configError, setConfigError] = React.useState<string>();
+  const [imageValid, setImageValid] = React.useState<boolean>(false);
+  const [userConfig, setUserConfig] = React.useState<UserConfigMapType>();
+
   React.useEffect(() => {
     let cancelled = false;
+
+    APIGet(CM_PATH).then((data: UserConfigMapType) => {
+      if (!cancelled) {
+        setUserConfig(data);
+      }
+    });
 
     APIGet(UI_CONFIG_PATH)
       .then((data: UiConfigType) => {
@@ -82,7 +91,7 @@ const App: React.FC = () => {
       );
     }
 
-    if (!uiConfig) {
+    if (!uiConfig || !userConfig) {
       return (
         <EmptyState variant={EmptyStateVariant.full}>
           <Spinner isSVG size="xl" />
@@ -92,12 +101,19 @@ const App: React.FC = () => {
 
     return (
       <>
-        <ImageForm uiConfig={uiConfig} />
-        <SizesForm uiConfig={uiConfig} />
-        {uiConfig.envVarConfig?.enabled !== false && <EnvVarForm uiConfig={uiConfig} />}
+        <ImageForm
+          uiConfig={uiConfig}
+          userConfig={userConfig}
+          onValidImage={() => setImageValid(true)}
+        />
+        <SizesForm uiConfig={uiConfig} userConfig={userConfig} />
+        {uiConfig.envVarConfig?.enabled !== false && (
+          <EnvVarForm uiConfig={uiConfig} userConfig={userConfig} />
+        )}
         <div className="jsp-spawner__buttons-bar">
           <input
             type="submit"
+            disabled={!imageValid}
             value="Start server"
             onClick={fireStartServerEvent}
             className="jsp-spawner__submit-button pf-c-button pf-m-primary"
