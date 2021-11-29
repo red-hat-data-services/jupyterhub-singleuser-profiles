0206020af67709ff26223d5e8e5b035184888ef2
Chad Roberts <croberts@redhat.com>
2021-11-29 13:21:40 -0500

    Revert "[UPSTREAM] Add spawn progress watch modal, replace spawn_pending page (#47)"
    This reverts commit 0ce24829d83f3a988b84ad26de9594afa4c27c0b.

--- jupyterhub_singleuser_profiles/ui/src/App/Spawner.tsx
+++ jupyterhub_singleuser_profiles/ui/src/App/Spawner.tsx
@@ -3,8 +3,6 @@ import '@patternfly/patternfly/patternfly.min.css';
 import '@patternfly/patternfly/patternfly-addons.css';
 import {
   Alert,
-  Button,
-  ButtonVariant,
   Title,
   EmptyState,
   EmptyStateVariant,
@@ -16,12 +14,10 @@ import { WarningTriangleIcon } from '@patternfly/react-icons';
 import ImageForm from '../ImageForm/ImageForm';
 import SizesForm from '../SizesForm/SizesForm';
 import EnvVarForm from '../EnvVarForm/EnvVarForm';
-import { HubUserRequest } from '../utils/HubCalls';
 import { APIGet } from '../utils/APICalls';
 import { CM_PATH, FOR_USER, INSTANCE_PATH, UI_CONFIG_PATH, USER } from '../utils/const';
 import { InstanceType, UiConfigType, UserConfigMapType } from '../utils/types';
-import { initSegment } from '../utils/segmentIOUtils';
-import StartServerModal from './StartServerModal';
+import { fireTrackingEvent, initSegment } from '../utils/segmentIOUtils';
 
 import './App.scss';
 
@@ -30,23 +26,9 @@ const Spawner: React.FC = () => {
   const [configError, setConfigError] = React.useState<string>();
   const [imageValid, setImageValid] = React.useState<boolean>(false);
   const [userConfig, setUserConfig] = React.useState<UserConfigMapType>();
-  const [startShown, setStartShown] = React.useState<boolean>(false);
-  const pageRef = React.useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     let cancelled = false;
-    HubUserRequest('GET')
-      .then((response) => {
-        return response?.json();
-      })
-      .then((results) => {
-        if (results.pending) {
-          setStartShown(true);
-        }
-      })
-      .catch((e) => {
-        console.error(e);
-      });
 
     APIGet(CM_PATH).then((data: UserConfigMapType) => {
       if (!cancelled) {
@@ -83,6 +65,20 @@ const Spawner: React.FC = () => {
       });
   }, []);
 
+  const fireStartServerEvent = () => {
+    APIGet(CM_PATH)
+      .then((data: UserConfigMapType) => {
+        fireTrackingEvent('Notebook Server Started', {
+          GPU: data.gpu,
+          lastSelectedSize: data.last_selected_size,
+          lastSelectedImage: data.last_selected_image,
+        });
+      })
+      .catch((e) => {
+        console.dir(e);
+      });
+  };
+
   const renderContent = () => {
     if (configError) {
       return (
@@ -106,27 +102,30 @@ const Spawner: React.FC = () => {
 
     return (
       <>
-        <ImageForm uiConfig={uiConfig} userConfig={userConfig} onValidImage={setImageValid} />
+        <ImageForm
+          uiConfig={uiConfig}
+          userConfig={userConfig}
+          onValidImage={() => setImageValid(true)}
+        />
         <SizesForm uiConfig={uiConfig} userConfig={userConfig} />
         {uiConfig.envVarConfig?.enabled !== false && (
           <EnvVarForm uiConfig={uiConfig} userConfig={userConfig} />
         )}
         <div className="jsp-spawner__buttons-bar">
-          <Button
-            variant={ButtonVariant.primary}
+          <input
+            type="submit"
             disabled={!imageValid}
-            className="jsp-app__spawner__submit-button"
-            onClick={() => setStartShown(true)}
-          >
-            Start Server
-          </Button>
+            value="Start server"
+            onClick={fireStartServerEvent}
+            className="jsp-spawner__submit-button pf-c-button pf-m-primary"
+          />
         </div>
       </>
     );
   };
 
   return (
-    <div className="jsp-app jsp-spawner" ref={pageRef}>
+    <div className="jsp-app jsp-spawner">
       <div className="jsp-app__header">
         {FOR_USER ? (
           <Alert
@@ -139,13 +138,6 @@ const Spawner: React.FC = () => {
         <div className="jsp-app__header__sub-title">Select options for your notebook server.</div>
       </div>
       {renderContent()}
-      {startShown ? (
-        <StartServerModal
-          pageRef={pageRef.current as HTMLElement}
-          shown={startShown}
-          onClose={() => setStartShown(false)}
-        />
-      ) : null}
     </div>
   );
 };
